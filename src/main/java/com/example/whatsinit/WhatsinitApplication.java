fab252d20e9bf2af6653bd5bc931853df34c43a3
Tolunay Mutlu <tlnymtlu@gmail.com>
2023-10-16 07:47:18 +0300

    init

---
+++ src/main/java/com/example/whatsinit/WhatsinitApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whatsinit/WhatsinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
