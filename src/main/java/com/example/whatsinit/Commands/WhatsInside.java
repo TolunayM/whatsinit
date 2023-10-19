b6ecedc28e002627a24910538c70e9a11354daf4
Tolunay Mutlu <tlnymtlu@gmail.com>
2023-10-19 19:59:41 +0300

    typo

--- src/main/java/com/example/whatsinit/Commands/WhatsInside.java
+++ src/main/java/com/example/whatsinit/Commands/WhatsInside.java
@@ -2,6 +2,7 @@ package com.example.whatsinit.Commands;
 
 import com.example.whatsinit.CommandController;
 import com.example.whatsinit.Contexts;
+import org.springframework.shell.command.CommandRegistration;
 import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.command.annotation.Option;
 
@@ -12,11 +13,13 @@ import java.awt.datatransfer.StringSelection;
 @Command()
 public class WhatsInside {
 
+
+
     private final CommandController commandController;
     private void copyToClipboard(String arg){
 
         System.setProperty("java.awt.headless","false");
-        String clip = "Check clipboard " + arg;
+        String clip = commandController.getByContext(arg);
         StringSelection stringSelection = new StringSelection(clip);
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection,null);
@@ -27,22 +30,22 @@ public class WhatsInside {
     }
 
     @Command(command = "web",description = "Web Dependencies")
-    public String webDependecies(){
-        return commandController.getByContext();
+    public String webDependencies(){
+        return commandController.getByContext(Contexts.web.name());
     }
 
     @Command(command = "security",description = "Security Dependencies")
-    public String securityDependecies(){
+    public String securityDependencies(){
         return commandController.getByContext(Contexts.security.name());
     }
 
     @Command(command ="data",description = "SQL-Data Dependencies")
-    public String sqlDependecies(){
+    public String sqlDependencies(){
         return commandController.getByContext(Contexts.data.name());
     }
 
     @Command(command = "template",description = "Template Dependencies")
-    public String templateDependecies(){
+    public String templateDependencies(){
         return commandController.getByContext(Contexts.template.name());
     }
 
@@ -52,10 +55,10 @@ public class WhatsInside {
         return "Jersey";
     }
 
-    @Command(command = "example",description = "aaaaa")
+    @Command(command = "copy",description = "Copy dependencies you want")
         public void exampleCopy(
-                @Option(longNames = "copy",shortNames = 'c' ,required = true) String arg){
-            copyToClipboard("jerseeey");
+                @Option(arity = CommandRegistration.OptionArity.ZERO_OR_MORE) String arg){
+            copyToClipboard(arg);
 
         }
     }
